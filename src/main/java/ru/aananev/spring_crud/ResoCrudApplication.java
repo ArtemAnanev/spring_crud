45858151e8c71438fd6d97484ae156f777ca3dc5
Artem Ananev <6167006@gmail.com>
2021-03-01 03:14:11 +0300

    first commit

---
+++ src/main/java/ru/aananev/spring_crud/ResoCrudApplication.java
@@ -0,0 +1,12 @@
+package ru.aananev.spring_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResoCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResoCrudApplication.class, args);
+    }
+}
